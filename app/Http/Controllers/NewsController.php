cb676535368cce06210805d6b02c44e370d37781
Nafhii <nafhidah.ramdhani1@gmail.com>
2020-11-19 22:15:24 +0700

    Initial commit

---
+++ app/Http/Controllers/NewsController.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+class NewsController extends Controller
+{
+    public function getData(){
+        $client = new Client();
+        $request = $client->get('https://newsapi.org/v2/top-headlines?country=id&apiKey=d4686b9b37c14d499434ea07edbf7103');
+        $response = $request->getBody();
+        $result = json_decode($response);
+        return view('home',['artikel'=>$result->articles]);
+}
+    public function searchData(Request $request){
+        $client = new Client();
+        $query = $request->keyword;
+        $req = $client->get('https://newsapi.org/v2/topheadlines?country=id&apiKey=YOURAPIKEY&q='.$query);
+        $response = $req->getBody();
+        $result = json_decode($response);
+        return view('home',['artikel'=>$result->articles]);
+   }
+   
+}
